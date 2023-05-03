c53ced8b18d1f4eec5f4afe38e30be34deacc209
Paulo Augusto <paulo-augusto12@outlook.com>
2023-05-03 09:00:49 -0300

    feat: developed request service class

---
+++ src/domain/services/requestService.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { IHttpService } from "../interfaces/http/IHttpService";
+import { HttpResponse } from "../models/httpResponse";
+
+export class RequestService implements IHttpService {
+  async getData (
+    url: string,
+    body: any,
+    config?: any,
+    method: "post" | "get"  = 'get'
+  ){
+    if(method === 'get'){
+        const response = await axios.get(url)
+
+        return new HttpResponse(response.data, response.status)
+    } else {
+        const response = await axios.post(url, body)
+
+        return new HttpResponse(response.data,response.status)
+    }
+  }
+  
+}
