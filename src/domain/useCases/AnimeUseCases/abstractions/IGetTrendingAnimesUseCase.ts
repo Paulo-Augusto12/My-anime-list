6899e5483b8cec25d52eb0ea99cb29ea84c9db4d
Paulo Augusto <paulo-augusto12@outlook.com>
2023-05-03 12:33:35 -0300

    feat: added request params to get trending anime use case interface

--- src/domain/useCases/AnimeUseCases/abstractions/IGetTrendingAnimesUseCase.ts
+++ src/domain/useCases/AnimeUseCases/abstractions/IGetTrendingAnimesUseCase.ts
@@ -1,6 +1,10 @@
 import { AnimeModel } from "../Models/AnimeModels";
 
-export interface IGetTrendingAnimeRequestParams {}
+export interface IGetTrendingAnimeRequestParams {
+  page: number;
+  limit: number;
+  type: "tv" | "movie" | "ova" | "special" | "ona" | "music";
+}
 
 export interface IGetTrendingAnimesUseCase {
   execute(params: IGetTrendingAnimeRequestParams): Promise<AnimeModel[]>;
