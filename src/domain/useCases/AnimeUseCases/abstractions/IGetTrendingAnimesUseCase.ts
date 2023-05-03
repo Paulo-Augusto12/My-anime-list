bc7e5f86cf77c3ec0d64f5229f117f9a4b5f56b4
Paulo Augusto <paulo-augusto12@outlook.com>
2023-05-03 13:00:26 -0300

    feat: added filter by prop on request params

--- src/domain/useCases/AnimeUseCases/abstractions/IGetTrendingAnimesUseCase.ts
+++ src/domain/useCases/AnimeUseCases/abstractions/IGetTrendingAnimesUseCase.ts
@@ -4,6 +4,7 @@ export interface IGetTrendingAnimeRequestParams {
   page: number;
   limit: number;
   type: "tv" | "movie" | "ova" | "special" | "ona" | "music";
+  filterBy: "airing" | "upcoming" | "bypopularity" | "favorite"
 }
 
 export interface IGetTrendingAnimesUseCase {
