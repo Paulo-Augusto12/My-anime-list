9f9d8d6f4d54ab7d83032607bf5ab7f4701cb110
Paulo Augusto <paulo-augusto12@outlook.com>
2023-05-03 08:52:15 -0300

    feat: developed initial version of animes repository

---
+++ src/data/AnimesRepository.ts
@@ -0,0 +1,20 @@
+import { AnimeDTO } from "../api/dto/AnimeDTO";
+import { IGetAllAnimeRepository } from "../domain/interfaces/AnimeRepository/IGetAllAnimeRepository";
+import { IHttpService } from "../domain/interfaces/http/IHttpService";
+import { HttpResponse } from "../domain/models/httpResponse";
+import { IGetAllAnimeRequestParams } from "../domain/useCases/AnimeUseCases/abstractions/IGetAllAnimeUseCase";
+
+export class AnimesRepository implements IGetAllAnimeRepository {
+  constructor(private httpService: IHttpService) {}
+
+  async getAllAnimes(
+    params: IGetAllAnimeRequestParams
+  ): Promise<HttpResponse<AnimeDTO>> {
+    const response = await this.httpService.getData(
+      "https://api.jikan.moe/v4/anime",
+      params
+    );
+
+    return response;
+  }
+}
