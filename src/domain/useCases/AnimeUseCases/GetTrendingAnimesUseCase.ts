ffbebc2372af79ac1e348e99cd8e37e482c1a3c4
Paulo Augusto <paulo-augusto12@outlook.com>
2023-05-03 12:26:15 -0300

    feat: developed get trending anime use case

---
+++ src/domain/useCases/AnimeUseCases/GetTrendingAnimesUseCase.ts
@@ -0,0 +1,24 @@
+import { IGetTrendingAnimesRepository } from "../../interfaces/AnimeRepository/IGetTrendingAnimesRepository";
+import { AnimeModel } from "./Models/AnimeModels";
+
+// Interface do caso de uso
+
+import {
+  IGetTrendingAnimeRequestParams,
+  IGetTrendingAnimesUseCase,
+} from "./abstractions/IGetTrendingAnimesUseCase";
+
+//
+
+export class GetTrendingAnimesUseCase implements IGetTrendingAnimesUseCase {
+  constructor(private repository: IGetTrendingAnimesRepository) {}
+
+  async execute(params: IGetTrendingAnimeRequestParams): Promise<AnimeModel[]> {
+    const response = await this.repository.getTrendingAnimes(params);
+
+    return response.data.data.map(
+      ({ title_english, images, episodes }) =>
+        new AnimeModel(title_english, images.jpg.image_url, episodes)
+    );
+  }
+}
