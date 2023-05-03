e53334d8005d406e8e7f95bedc6c18caf158d1a7
Paulo Augusto <paulo-augusto12@outlook.com>
2023-05-03 14:26:07 -0300

    feat: developed initial full anime model

---
+++ src/domain/useCases/AnimeUseCases/Models/FullAnime.ts
@@ -0,0 +1,9 @@
+export class FullAnime {
+  constructor(
+    readonly name: string = "",
+    readonly sinopsys: string = "",
+    readonly airing: boolean = false,
+    readonly popularityRanking: number = 0,
+    readonly score: number = 0
+  ) {}
+}
