56a0e1640a55cdf02aa76ef001131b7d371654c7
Paulo Augusto <paulo-augusto12@outlook.com>
2023-05-04 21:09:06 -0300

    feat: developed application first test

---
+++ __tests__/fakeAnimeRepository.ts
@@ -0,0 +1,20 @@
+import { GetAllAnimesDTO } from "../src/domain/dto/GetAllAnimesDTO";
+import { IGetAllAnimeRepository } from "../src/domain/interfaces/AnimeRepository/IGetAllAnimeRepository";
+import { IHttpService } from "../src/domain/interfaces/http/IHttpService";
+import { HttpResponse } from "../src/domain/models/httpResponse";
+import { IGetAllAnimeRequestParams } from "../src/domain/useCases/AnimeUseCases/abstractions/IGetAllAnimeUseCase";
+
+export class FakeAnimeRepository implements IGetAllAnimeRepository {
+  constructor(private httpService: IHttpService) {}
+
+  async getAllAnimes(
+    params: IGetAllAnimeRequestParams
+  ): Promise<HttpResponse<GetAllAnimesDTO>> {
+    const response = await this.httpService.getData(
+      `https://api.jikan.moe/v4/anime?page=${params.page}&limit=${params.limit}`,
+      params
+    );
+
+    return response;
+  }
+}
