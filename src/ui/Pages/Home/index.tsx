7bbbb7c746ca7fc892596dcd0ec89aed97ea923a
Paulo Augusto <paulo-augusto12@outlook.com>
2023-05-06 10:49:42 -0300

    feat: added view all anime button

--- src/ui/Pages/Home/index.tsx
+++ src/ui/Pages/Home/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { useHome } from "./useHome";
-import { Box, Skeleton, Typography } from "@mui/material";
+import { Box, Button, Skeleton, Typography } from "@mui/material";
 import { Header } from "../../Components/Header";
 import { Card } from "../../Components/Card";
+import { ArrowRight } from "@phosphor-icons/react";
 
 export function Home() {
   const hook = useHome();
   return (
-    <Box>
+    <Box sx={{ display: "flex", flexDirection: "column", gap: "1.5rem" }}>
       <Header
         animeQueryValue={hook.animeQuery}
         setSearchBarAnimeQueryValue={(e) => {
@@ -52,42 +53,66 @@ export function Home() {
         </Box>
       </Box>
       {hook.loadingTrendingAnimes ? (
-        <Skeleton sx={{height: '404px', borderRadius: '16px'}}/>
-      ): (
-
-      <Box
-        sx={{
-          marginTop: "2rem",
-          display: "flex",
-          flexDirection: "column",
-          gap: "1rem",
-        }}
-      >
-        <Typography color="black" fontWeight={700}>Trending</Typography>
+        <Skeleton sx={{ height: "404px", borderRadius: "16px" }} />
+      ) : (
         <Box
           sx={{
+            marginTop: "2rem",
             display: "flex",
-            flexDirection: "row",
+            flexDirection: "column",
             gap: "1rem",
-            alignItems: "flex-start",
-            justifyContent: "space-between",
           }}
         >
-          {hook.trendingAnimes.map(({ episodes, name, photo }) => (
-            <Box maxWidth={"245px"}>
-              {!photo.trim() ? (
-                <Skeleton />
-              ) : (
-                <Card
-                  animeName={name}
-                  animeEpisodesQtde={episodes}
-                  animePhoto={photo}
-                />
-              )}
-            </Box>
-          ))}
+          <Typography color="black" fontWeight={700}>
+            Trending
+          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              gap: "1rem",
+              alignItems: "flex-start",
+              justifyContent: "space-between",
+            }}
+          >
+            {hook.trendingAnimes.map(({ episodes, name, photo }) => (
+              <Box maxWidth={"245px"}>
+                {!photo.trim() ? (
+                  <Skeleton />
+                ) : (
+                  <Card
+                    animeName={name}
+                    animeEpisodesQtde={episodes}
+                    animePhoto={photo}
+                  />
+                )}
+              </Box>
+            ))}
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              marginTop: "2rem",
+            }}
+          >
+            <Button
+              sx={{
+                height: "25px",
+                backgroundColor: "#d7d7d7",
+                padding: "1.8rem",
+                color: "black",
+                width: "15%",
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              View All Anime
+              <ArrowRight size={16} />
+            </Button>
+          </Box>
         </Box>
-      </Box>
       )}
     </Box>
   );
