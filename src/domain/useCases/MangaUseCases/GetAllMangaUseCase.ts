145dbfc0e279f270313eb6bf786bf725757efa0d
Paulo Augusto <paulo-augusto12@outlook.com>
2023-05-08 12:29:07 -0300

    feat: use case returning correct page data

--- src/domain/useCases/MangaUseCases/GetAllMangaUseCase.ts
+++ src/domain/useCases/MangaUseCases/GetAllMangaUseCase.ts
@@ -4,6 +4,8 @@ import {
   IGetAllMangaResponse,
   IGetAllMangaUseCase,
 } from "./abstractions/IGetAllMangaUseCase";
+import { Manga } from "./models/Manga";
+import { Pagination } from "./models/Pagination";
 
 export class GetAllMangaUseCase implements IGetAllMangaUseCase {
   constructor(private repository: IGetAllMangaRepository) {}
@@ -13,6 +15,18 @@ export class GetAllMangaUseCase implements IGetAllMangaUseCase {
   ): Promise<IGetAllMangaResponse> {
     const response = await this.repository.getAllManga(params);
 
-    return {};
+    const mangas = response.data.data.map(
+      ({ title, images, chapters }) =>
+        new Manga(title, images.webp.image_url, chapters)
+    );
+    const paginationData = new Pagination(
+      params.page,
+      response.data.pagination.last_visible_page,
+      response.data.pagination.items.total
+    );
+    return {
+      mangas,
+      paginationData,
+    };
   }
 }
