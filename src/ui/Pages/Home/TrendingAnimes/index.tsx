2815ce53b081563ec96491d8ca3783d3ab651be7
Paulo Augusto <paulo-augusto12@outlook.com>
2023-05-08 09:06:28 -0300

    refactor: rendering hero text and image on trending anime component

--- src/ui/Pages/Home/TrendingAnimes/index.tsx
+++ src/ui/Pages/Home/TrendingAnimes/index.tsx
@@ -1,8 +1,54 @@
 import React from "react";
 
-import { useTrendingAnimes } from "./useTrendingAnimes";
-export function TrendingAnimes() {
-  const { actions, states } = useTrendingAnimes();
+// Material ui components
 
-  return <></>;
+import { Box, Skeleton, Typography } from "@mui/material";
+
+//
+
+// Hook
+
+import { useTrendingAnimes } from "./useTrendingAnimes";
+
+//
+
+export function TrendingAnimes() {
+  const { actions, states, visualElements } = useTrendingAnimes();
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        gap: "2rem",
+        alignItems: "center",
+      }}
+    >
+      <Typography variant="h1" color="#4361ee">
+        My Anime List
+      </Typography>
+      <Box
+        sx={{
+          height: "100px",
+          borderRadius: "55px",
+        }}
+      >
+        {!visualElements.characterPhoto.trim() ? (
+          <Skeleton
+            sx={{ borderRadius: "33px", width: "156px", height: "100px" }}
+          />
+        ) : (
+          <img
+            src={visualElements.characterPhoto}
+            style={{
+              objectFit: "fill",
+              width: "156px",
+              height: "100px",
+              borderRadius: "55px",
+            }}
+          />
+        )}
+      </Box>
+    </Box>
+  );
 }
--- src/ui/Pages/Home/TrendingAnimes/useTrendingAnimes.tsx
+++ src/ui/Pages/Home/TrendingAnimes/useTrendingAnimes.tsx
@@ -1,8 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { RequestService } from "../../../../domain/services/requestService";
+import { AnimesRepository } from "../../../../data/AnimesRepository";
+import { GetARandomCharacterUseCase } from "../../../../domain/useCases/CharacterUseCase/GetARandomCharacterUseCase";
+import { CharactersRepository } from "../../../../data/CharactersRepository";
 
 export function useTrendingAnimes() {
+  // ------------------------ Depencency injection ------------------------ //
+
+  // Http service
+
+  const httpService = new RequestService();
+
+  //
+
+  // Repositories
+
+  const charactersRepository = new CharactersRepository(httpService);
+  const animesRepository = new AnimesRepository(httpService);
+
+  //
+
+  // Use Cases
+
+  const getARandomCharacterUseCase = new GetARandomCharacterUseCase(
+    charactersRepository
+  );
+
+  //
+
+  // ------------------------ Depencency injection ------------------------ //
+
+  const [characterPhoto, setCharacterPhoto] = useState("");
+
+  async function randomCharacterPhoto() {
+    const data = await getARandomCharacterUseCase.execute();
+
+    setCharacterPhoto(data.photo)
+  }
+
+  useEffect(() => {
+    if(!characterPhoto.trim()){
+      randomCharacterPhoto()
+    }
+  }, [])
+
   return {
     states: {},
     actions: {},
+    visualElements: {
+      characterPhoto,
+    },
   };
 }
