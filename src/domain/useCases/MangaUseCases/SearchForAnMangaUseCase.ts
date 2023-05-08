669376df93a831d127521aa925ad516d8070d856
Paulo Augusto <paulo-augusto12@outlook.com>
2023-05-08 15:03:32 -0300

    feat: imported use case interfaces

--- src/domain/useCases/MangaUseCases/SearchForAnMangaUseCase.ts
+++ src/domain/useCases/MangaUseCases/SearchForAnMangaUseCase.ts
@@ -1,15 +1,31 @@
+// use case interfaces
+
+import {
+  ISearchForAnMangaRequestParams,
+  ISearchForAnMangaResponse,
+  ISearchForAnMangaUseCase,
+} from "./abstractions/ISearchForAnMangaUseCase";
+
+//
+
+// Models
+
 import { Manga } from "./models/Manga";
 import { Pagination } from "./models/Pagination";
 
-export class SearchForAnMangaUseCase implements ISearchForAnMangaUseCase {
-    constructor(private repository: ISearchForAnMangaRepository){}
-    
-    async execute(params: ISearchForAnMangaRequestParams): Promise <ISearchForAnMangaResponse>{
-        const response = await this.repository.searchForAnAnime(params);
+//
 
-        return {
-            mangas: Manga,
-            pagination: Pagination
-        }
-    }
+export class SearchForAnMangaUseCase implements ISearchForAnMangaUseCase {
+  constructor(private repository: ISearchForAnMangaRepository) {}
+
+  async execute(
+    params: ISearchForAnMangaRequestParams
+  ): Promise<ISearchForAnMangaResponse> {
+    const response = await this.repository.searchForAnAnime(params);
+
+    return {
+      mangas: Manga,
+      pagination: Pagination,
+    };
+  }
 }
