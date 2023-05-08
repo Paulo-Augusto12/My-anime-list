ecf24c7b1204fb24013b825d6e7e4f6e53460d5c
Paulo Augusto <paulo-augusto12@outlook.com>
2023-05-08 14:17:21 -0300

    feat: added correct return for use case

--- src/domain/useCases/MangaUseCases/GetTrendingMangasUseCase.ts
+++ src/domain/useCases/MangaUseCases/GetTrendingMangasUseCase.ts
@@ -1,24 +1,33 @@
-
 // Models
 
-import { IGetTrendingMangasRequestParams, IGetTrendingMangasResponse, IGetTrendingMangasUseCase } from "./abstractions/IGetTrendingMangasUseCase"
-import { Manga } from "./models/Manga"
-import { Pagination } from "./models/Pagination"
+import { IGetTrendingMangasRepository } from "../../interfaces/MangaRepository/IGetTrendingMangasRepository";
+import {
+  IGetTrendingMangasRequestParams,
+  IGetTrendingMangasResponse,
+  IGetTrendingMangasUseCase,
+} from "./abstractions/IGetTrendingMangasUseCase";
+import { Manga } from "./models/Manga";
+import { Pagination } from "./models/Pagination";
 
 //
 
 export class GetTrendingMangasUseCase implements IGetTrendingMangasUseCase {
-    constructor(private repository: IGetTrendingMangasRepository){}
+  constructor(private repository: IGetTrendingMangasRepository) {}
 
-    async execute(params: IGetTrendingMangasRequestParams): Promise <IGetTrendingMangasResponse> {
-        const response = await this.repository.getTrendingMangas(params)
+  async execute(
+    params: IGetTrendingMangasRequestParams
+  ): Promise<IGetTrendingMangasResponse> {
+    const response = await this.repository.getTrendingMangas(params);
 
-        const mangas = new Manga()
-        const pagination = new Pagination()
+    const mangas = response.data.data.map(
+      ({ title, images, chapters }) =>
+        new Manga(title, images.webp.image_url, chapters)
+    );
+    const pagination = new Pagination();
 
-        return {
-            mangas,
-            pagination
-        }
-    }
+    return {
+      mangas,
+      pagination,
+    };
+  }
 }
