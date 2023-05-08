24df422a3a023a150bf27835caab904935ca7ee4
Paulo Augusto <paulo-augusto12@outlook.com>
2023-05-08 13:02:53 -0300

    feat: implemented getARandomManga Interface on Mangas Repository

--- src/data/MangasRepository.ts
+++ src/data/MangasRepository.ts
@@ -20,10 +20,14 @@ import { IGetAllMangaRequestParams } from "../domain/useCases/MangaUseCases/abst
 
 import { IHttpService } from "../domain/interfaces/http/IHttpService";
 import { HttpResponse } from "../domain/models/httpResponse";
+import { IGetARandomAnimeRepository } from "../domain/interfaces/AnimeRepository/IGetARandomAnimeRepository";
+import { GetARandomAnimeDTO } from "../domain/dto/GetARandomAnimeDTO";
 
 //
 
-export class MangasRepository implements IGetAllMangaRepository {
+export class MangasRepository
+  implements IGetAllMangaRepository, IGetARandomAnimeRepository
+{
   constructor(private httpService: IHttpService) {}
 
   async getAllManga({
@@ -33,6 +37,15 @@ export class MangasRepository implements IGetAllMangaRepository {
     const response = await this.httpService.getData(
       `https://api.jikan.moe/v4/manga?page=${page}&limit=${limit}`,
       { limit, page }
+    );
+
+    return response;
+  }
+
+  async getARandomAnime(): Promise<HttpResponse<GetARandomAnimeDTO>> {
+    const response = await this.httpService.getData(
+      "https://api.jikan.moe/v4/random/manga",
+      {}
     );
 
     return response;
