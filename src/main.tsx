463bfa3f403fa0f469c378fdaf78750cb053b966
matlencar <rm94642@fiap.com.br>
2023-05-08 14:35:43 -0300

    refactor: returning to the initial state of the app

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { Box } from "@mui/material";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from '../src/ui/Pages/Home/'
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Box sx={{ padding: "1rem" }}>
-        <App />
-      </Box>
-    </BrowserRouter>
+     <App />
   </React.StrictMode>
 );
