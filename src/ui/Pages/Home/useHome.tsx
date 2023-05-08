e0047acb451bd864b36a8830918b67f637de8a16
Paulo Augusto <paulo-augusto12@outlook.com>
2023-05-08 10:21:49 -0300

    refactor: removed unnecessary code from the page hook

--- src/ui/Pages/Home/useHome.tsx
+++ src/ui/Pages/Home/useHome.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import { AnimeModel } from "../../../domain/useCases/AnimeUseCases/Models/AnimeModels";
+import React, { useState } from "react";
 
 // Services
 
@@ -16,7 +15,6 @@ import { AnimesRepository } from "../../../data/AnimesRepository";
 // Use cases
 
 import { SearchForAnAnimeUseCase } from "../../../domain/useCases/AnimeUseCases/SearchForAnAnimeUseCase";
-import { GetAllAnimeUseCase } from "../../../domain/useCases/AnimeUseCases/GetAllAnimesUseCase";
 
 //
 
@@ -32,12 +30,6 @@ const animeRepository = new AnimesRepository(httpService);
 
 //
 
-//  getAllAnimesUseCase
-
-const getAnimeUseCase = new GetAllAnimeUseCase(animeRepository);
-
-//
-
 // searchForAnAnimeUseCase
 
 const searchForAnAnimeUseCase = new SearchForAnAnimeUseCase(animeRepository);
@@ -45,41 +37,16 @@ const searchForAnAnimeUseCase = new SearchForAnAnimeUseCase(animeRepository);
 //
 
 export function useHome() {
-  const [animes, setAnimes] = useState<AnimeModel[]>([]);
   const [animeQuery, setAnimeQuery] = useState("");
   const [loadingAnimes, setLoadingAnimes] = useState(false);
-  const [page, setPage] = useState(1);
-  const [paginationData, setpaginationData] = useState({
-    page: 0,
-    totalOfPages: 0,
-  });
 
-  async function getAnimes() {
-    setLoadingAnimes(true);
-    const params = {
-      page: page,
-      limit: 24,
-    };
-    try {
-      const data = await getAnimeUseCase.execute(params);
-      setAnimes(data.animes);
-      setpaginationData({
-        page: page,
-        totalOfPages: data.paginationInfo.lastPage,
-      });
-      setLoadingAnimes(false);
-    } catch (err) {
-      setLoadingAnimes(false);
-      console.log(JSON.stringify(err));
-    }
-  }
 
   async function searchAnime() {
     setLoadingAnimes(true);
     try {
       const data = await searchForAnAnimeUseCase.execute(animeQuery);
 
-      setAnimes(data);
+      // setAnimes(data);
       setAnimeQuery("");
       setLoadingAnimes(false);
     } catch (err) {
@@ -88,19 +55,12 @@ export function useHome() {
     }
   }
 
-  useEffect(() => {
-    getAnimes();
-  }, [page]);
 
 
   return {
-    animes,
     animeQuery,
     setAnimeQuery,
     searchAnime,
-    page,
-    setPage,
-    paginationData,
     loadingAnimes,
   };
 }
