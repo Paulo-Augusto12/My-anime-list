c6fc046226d68c251736a03b990e8b7cb571634b
Paulo Augusto <paulo-augusto12@outlook.com>
2023-05-08 15:08:03 -0300

    chore: use case returning the correct data for the page

--- src/domain/useCases/MangaUseCases/SearchForAnMangaUseCase.ts
+++ src/domain/useCases/MangaUseCases/SearchForAnMangaUseCase.ts
@@ -1,5 +1,6 @@
 // use case interfaces
 
+import { ISearchForAnMangaRepository } from "../../interfaces/MangaRepository/ISearchForAnMangaRepository";
 import {
   ISearchForAnMangaRequestParams,
   ISearchForAnMangaResponse,
@@ -21,11 +22,23 @@ export class SearchForAnMangaUseCase implements ISearchForAnMangaUseCase {
   async execute(
     params: ISearchForAnMangaRequestParams
   ): Promise<ISearchForAnMangaResponse> {
-    const response = await this.repository.searchForAnAnime(params);
+    const response = await this.repository.searchForAnManga(params);
 
+    const mangas = response.data.data.map(
+      ({ title, images, chapters }) =>
+        new Manga(title, images.webp.image_url, chapters)
+    );
+
+    const pagination_data = response.data.pagination;
+
+    const pagination = new Pagination(
+      params.page,
+      pagination_data.last_visible_page,
+      pagination_data.items.per_page
+    );
     return {
-      mangas: Manga,
-      pagination: Pagination,
+      mangas,
+      pagination,
     };
   }
 }
