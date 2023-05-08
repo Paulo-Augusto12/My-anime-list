8b017e1701004fb684e4a69e8c96e7f545695192
Paulo Augusto <paulo-augusto12@outlook.com>
2023-05-08 14:36:01 -0300

    feat: implemented  getTrendingMangas repository interface

--- src/data/MangasRepository.ts
+++ src/data/MangasRepository.ts
@@ -22,11 +22,17 @@ import { IGetAllMangaRequestParams } from "../domain/useCases/MangaUseCases/abst
 
 import { IHttpService } from "../domain/interfaces/http/IHttpService";
 import { HttpResponse } from "../domain/models/httpResponse";
+import { IGetTrendingMangasRepository } from "../domain/interfaces/MangaRepository/IGetTrendingMangasRepository";
+import { GetTrendingMangasDTO } from "../domain/dto/GetTrendingMangasDTO";
+import { IGetTrendingMangasRequestParams } from "../domain/useCases/MangaUseCases/abstractions/IGetTrendingMangasUseCase";
 
 //
 
 export class MangasRepository
-  implements IGetAllMangaRepository, IGetARandomAnimeRepository
+  implements
+    IGetAllMangaRepository,
+    IGetARandomAnimeRepository,
+    IGetTrendingMangasRepository
 {
   constructor(private httpService: IHttpService) {}
 
@@ -48,6 +54,26 @@ export class MangasRepository
       {}
     );
 
+    return response;
+  }
+
+  async getTrendingMangas({
+    filter,
+    limit,
+    page,
+    type,
+  }: IGetTrendingMangasRequestParams): Promise<
+    HttpResponse<GetTrendingMangasDTO>
+  > {
+    const response = await this.httpService.getData(
+      `https://api.jikan.moe/v4/top/manga?type=${type}&filter=${filter}&page=${page}&limit=${limit}`,
+      {
+        filter,
+        limit,
+        page,
+        type,
+      }
+    );
     return response;
   }
 }
