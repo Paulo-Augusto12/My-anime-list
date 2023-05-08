dc3f3cd0d2fa46e97c516550efbd3a543f77ff2a
Paulo Augusto <paulo-augusto12@outlook.com>
2023-05-08 09:17:10 -0300

    refactor: trending animes being rendered by respective component

--- src/ui/Pages/Home/TrendingAnimes/index.tsx
+++ src/ui/Pages/Home/TrendingAnimes/index.tsx
@@ -9,6 +9,7 @@ import { Box, Skeleton, Typography } from "@mui/material";
 // Hook
 
 import { useTrendingAnimes } from "./useTrendingAnimes";
+import { Card } from "../../../Components/Card";
 
 //
 
@@ -16,38 +17,71 @@ export function TrendingAnimes() {
   const { actions, states, visualElements } = useTrendingAnimes();
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "row",
-        gap: "2rem",
-        alignItems: "center",
-      }}
-    >
-      <Typography variant="h1" color="#4361ee">
-        My Anime List
-      </Typography>
+    <Box sx={{ display: "flex", flexDirection: "column", gap: "2rem" }}>
       <Box
         sx={{
-          height: "100px",
-          borderRadius: "55px",
+          display: "flex",
+          flexDirection: "row",
+          gap: "2rem",
+          alignItems: "center",
         }}
       >
-        {!visualElements.characterPhoto.trim() ? (
-          <Skeleton
-            sx={{ borderRadius: "33px", width: "156px", height: "100px" }}
-          />
-        ) : (
-          <img
-            src={visualElements.characterPhoto}
-            style={{
-              objectFit: "fill",
-              width: "156px",
-              height: "100px",
-              borderRadius: "55px",
-            }}
-          />
-        )}
+        <Typography variant="h1" color="#4361ee">
+          My Anime List
+        </Typography>
+        <Box
+          sx={{
+            height: "100px",
+            borderRadius: "55px",
+          }}
+        >
+          {!visualElements.characterPhoto.trim() ? (
+            <Skeleton
+              sx={{ borderRadius: "33px", width: "156px", height: "100px" }}
+            />
+          ) : (
+            <img
+              src={visualElements.characterPhoto}
+              style={{
+                objectFit: "fill",
+                width: "156px",
+                height: "100px",
+                borderRadius: "55px",
+              }}
+            />
+          )}
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          marginTop: "2rem",
+          display: "flex",
+          flexDirection: "column",
+          gap: "1rem",
+        }}
+      >
+        <Typography variant="h5" fontWeight={700}>
+          Trending
+        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          {visualElements.trendingAnimes.map(
+            ({ name, photo, episodes, descrition }, index) => (
+              <Card
+                animeName={name}
+                animeEpisodesQtde={episodes}
+                animePhoto={photo}
+                key={index}
+              />
+            )
+          )}
+        </Box>
       </Box>
     </Box>
   );
--- src/ui/Pages/Home/TrendingAnimes/useTrendingAnimes.tsx
+++ src/ui/Pages/Home/TrendingAnimes/useTrendingAnimes.tsx
@@ -1,8 +1,31 @@
 import React, { useEffect, useState } from "react";
+
+// service
+
 import { RequestService } from "../../../../domain/services/requestService";
+
+//
+
+// repositories
+
 import { AnimesRepository } from "../../../../data/AnimesRepository";
-import { GetARandomCharacterUseCase } from "../../../../domain/useCases/CharacterUseCase/GetARandomCharacterUseCase";
 import { CharactersRepository } from "../../../../data/CharactersRepository";
+
+//
+
+// use cases
+
+import { GetARandomCharacterUseCase } from "../../../../domain/useCases/CharacterUseCase/GetARandomCharacterUseCase";
+import { GetTrendingAnimesUseCase } from "../../../../domain/useCases/AnimeUseCases/GetTrendingAnimesUseCase";
+
+//
+
+// Request params
+
+import { IGetTrendingAnimeRequestParams } from "../../../../domain/useCases/AnimeUseCases/abstractions/IGetTrendingAnimesUseCase";
+import { AnimeModel } from "../../../../domain/useCases/AnimeUseCases/Models/AnimeModels";
+
+//
 
 export function useTrendingAnimes() {
   // ------------------------ Depencency injection ------------------------ //
@@ -25,30 +48,53 @@ export function useTrendingAnimes() {
   const getARandomCharacterUseCase = new GetARandomCharacterUseCase(
     charactersRepository
   );
+  const getTrendingAnimesUseCase = new GetTrendingAnimesUseCase(
+    animesRepository
+  );
 
   //
 
   // ------------------------ Depencency injection ------------------------ //
 
   const [characterPhoto, setCharacterPhoto] = useState("");
+  const [trendingAnimes, setTrendingAnimes] = useState<AnimeModel[]>([]);
 
   async function randomCharacterPhoto() {
     const data = await getARandomCharacterUseCase.execute();
 
-    setCharacterPhoto(data.photo)
+    setCharacterPhoto(data.photo);
+  }
+
+  async function getTrendingAnimes() {
+    const params: IGetTrendingAnimeRequestParams = {
+      page: 1,
+      limit: 5,
+      type: "tv",
+      filterBy: "bypopularity",
+    };
+    const data = await getTrendingAnimesUseCase.execute(params);
+
+    setTrendingAnimes(data);
   }
 
   useEffect(() => {
-    if(!characterPhoto.trim()){
-      randomCharacterPhoto()
+    if (!characterPhoto.trim()) {
+      randomCharacterPhoto();
     }
-  }, [])
+  }, []);
+
+  useEffect(() => {
+    if (!trendingAnimes.length || trendingAnimes === undefined) {
+      getTrendingAnimes();
+    }
+  }, []);
 
   return {
     states: {},
     actions: {},
     visualElements: {
       characterPhoto,
+      trendingAnimes
     },
   };
 }
