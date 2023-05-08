b947e5c05990d8066f30e7fe5c1f2e6b0244335b
Paulo Augusto <paulo-augusto12@outlook.com>
2023-05-08 09:25:29 -0300

    refactor: removed unnecessary code from the useHome hook

--- src/ui/Pages/Home/useHome.tsx
+++ src/ui/Pages/Home/useHome.tsx
@@ -10,22 +10,13 @@ import { RequestService } from "../../../domain/services/requestService";
 // Repositories
 
 import { AnimesRepository } from "../../../data/AnimesRepository";
-import { CharactersRepository } from "../../../data/CharactersRepository";
 
 //
 
 // Use cases
 
 import { SearchForAnAnimeUseCase } from "../../../domain/useCases/AnimeUseCases/SearchForAnAnimeUseCase";
-import { GetTrendingAnimesUseCase } from "../../../domain/useCases/AnimeUseCases/GetTrendingAnimesUseCase";
 import { GetAllAnimeUseCase } from "../../../domain/useCases/AnimeUseCases/GetAllAnimesUseCase";
-import { GetARandomCharacterUseCase } from "../../../domain/useCases/CharacterUseCase/GetARandomCharacterUseCase";
-
-//
-
-// Params
-
-import { IGetTrendingAnimeRequestParams } from "../../../domain/useCases/AnimeUseCases/abstractions/IGetTrendingAnimesUseCase";
 
 //
 
@@ -41,12 +32,6 @@ const animeRepository = new AnimesRepository(httpService);
 
 //
 
-// Characters Repository
-
-const charactersRepository = new CharactersRepository(httpService);
-
-//
-
 //  getAllAnimesUseCase
 
 const getAnimeUseCase = new GetAllAnimeUseCase(animeRepository);
@@ -59,26 +44,9 @@ const searchForAnAnimeUseCase = new SearchForAnAnimeUseCase(animeRepository);
 
 //
 
-// getTrendingAnimesUseCase
-
-const getTrendingAnimesUseCase = new GetTrendingAnimesUseCase(animeRepository);
-
-//
-
-// getARandomCharacterUseCase
-
-const getARandomCharacterUseCase = new GetARandomCharacterUseCase(
-  charactersRepository
-);
-
-//
-
 export function useHome() {
   const [animes, setAnimes] = useState<AnimeModel[]>([]);
-  const [trendingAnimes, setTrendingAnimes] = useState<AnimeModel[]>([]);
   const [animeQuery, setAnimeQuery] = useState("");
-  const [randomCharacterPhoto, setRandomCharacterPhoto] = useState("");
-  const [loadingTrendingAnimes, setLoadingTrendingAnimes] = useState(false);
   const [loadingAnimes, setLoadingAnimes] = useState(false);
   const [page, setPage] = useState(1);
   const [paginationData, setpaginationData] = useState({
@@ -120,56 +88,16 @@ export function useHome() {
     }
   }
 
-  async function getTrendingAnimes() {
-    setLoadingTrendingAnimes(true);
-    try {
-      const params: IGetTrendingAnimeRequestParams = {
-        page: 1,
-        limit: 5,
-        type: "tv",
-        filterBy: "bypopularity",
-      };
-
-      const data = await getTrendingAnimesUseCase.execute(params);
-
-      setTrendingAnimes(data);
-      setLoadingTrendingAnimes(false);
-    } catch (err) {
-      setLoadingTrendingAnimes(false);
-      console.log(JSON.stringify(err));
-    }
-  }
-
-  async function getARandomCharacterPhoto() {
-    const data = await getARandomCharacterUseCase.execute();
-
-    setRandomCharacterPhoto(data.photo);
-  }
-
   useEffect(() => {
     getAnimes();
   }, [page]);
 
-  useEffect(() => {
-    if (!trendingAnimes.length) {
-      getTrendingAnimes();
-    }
-  }, []);
-
-  useEffect(() => {
-    if (!randomCharacterPhoto.trim()) {
-      getARandomCharacterPhoto();
-    }
-  }, []);
 
   return {
     animes,
     animeQuery,
     setAnimeQuery,
     searchAnime,
-    randomCharacterPhoto,
-    trendingAnimes,
-    loadingTrendingAnimes,
     page,
     setPage,
     paginationData,
