48b6844787329f6ff87e7f0c985cc41882c3179e
Paulo Augusto <paulo-augusto12@outlook.com>
2023-05-08 10:16:34 -0300

    refactor: all animes now are rendered by his respective component

--- src/ui/Pages/Home/AllAnimes/index.tsx
+++ src/ui/Pages/Home/AllAnimes/index.tsx
@@ -1,7 +1,54 @@
 import React from "react";
 
 import { useAllAnimes } from "./useAllAnimes";
+import { Box, Grid, Skeleton, Typography } from "@mui/material";
+import { Card } from "../../../Components/Card";
 export function AllAnimes() {
   const { actions, states } = useAllAnimes();
-  return <></>;
+  return (
+    <Box>
+      {!states.animes.length ? (
+        <Skeleton
+          height={"1607px"}
+          variant="rectangular"
+          sx={{ borderRadius: "18px" }}
+          id="animes"
+        />
+      ) : (
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "1rem",
+            marginTop: "18px",
+          }}
+        >
+          <Typography variant="h5" fontWeight={700}>
+            Animes
+          </Typography>
+          <Grid
+            container
+            spacing={4}
+            rowSpacing={8}
+            columnSpacing={20}
+            columns={32}
+            id="animes"
+            sx={{ scrollBehavior: "smooth", marginTop: "18px" }}
+          >
+            {states.animes.map(
+              ({ name, episodes, photo, descrition }, index) => (
+                <Grid item key={index} xs={8} sx={{ maxWidth: "255px" }}>
+                  <Card
+                    animeName={name}
+                    animeEpisodesQtde={episodes}
+                    animePhoto={photo}
+                  />
+                </Grid>
+              )
+            )}
+          </Grid>
+        </Box>
+      )}
+    </Box>
+  );
 }
--- src/ui/Pages/Home/AllAnimes/useAllAnimes.tsx
+++ src/ui/Pages/Home/AllAnimes/useAllAnimes.tsx
@@ -1,8 +1,87 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+// service
+
+import { RequestService } from "../../../../domain/services/requestService";
+
+//
+
+// repositories
+
+import { AnimesRepository } from "../../../../data/AnimesRepository";
+
+//
+
+// use cases
+
+import { GetAllAnimeUseCase } from "../../../../domain/useCases/AnimeUseCases/GetAllAnimesUseCase";
+
+//
+
+// Request params
+
+import { IGetAllAnimeRequestParams } from "../../../../domain/useCases/AnimeUseCases/abstractions/IGetAllAnimeUseCase";
+import { AnimeModel } from "../../../../domain/useCases/AnimeUseCases/Models/AnimeModels";
+
+//
+
+// ------------------------ Depencency injection ------------------------ //
+
+// Http service
+
+const httpService = new RequestService();
+
+//
+
+// Repositories
+
+const animesRepository = new AnimesRepository(httpService);
+
+//
+
+// Use Cases
+
+const getAllAnimesUseCase = new GetAllAnimeUseCase(animesRepository);
+
+//
+
+// ------------------------ Depencency injection ------------------------ //
 
 export function useAllAnimes() {
+  const [animes, setAnimes] = useState<AnimeModel[]>([]);
+  const [paginationInfo, setPaginationInfo] = useState({
+    lastPage: 0,
+    hasNextPage: false,
+    currentPage: 0,
+    items: {
+      count: 0,
+      total: 0,
+      perPage: 0,
+    },
+  });
+
+  async function getAllAnimes() {
+    const params: IGetAllAnimeRequestParams = {
+      page: 1,
+      limit: 24,
+    };
+    const data = await getAllAnimesUseCase.execute(params);
+
+    setAnimes(data.animes);
+    setPaginationInfo(data.paginationInfo);
+  }
+
+  useEffect(() => {
+    if (!animes.length || animes === undefined) {
+      getAllAnimes();
+    }
+  }, []);
+
   return {
-    states: {},
+    states: {
+      animes,
+      paginationInfo,
+    },
     actions: {},
   };
 }
