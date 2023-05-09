e357c02eb7b833eb9cce0a957222c3245fc002ff
matlencar <rm94642@fiap.com.br>
2023-05-09 11:22:10 -0300

    feat: applying styling layer in the mangaHome

--- src/ui/Pages/HomeMangas/index.tsx
+++ src/ui/Pages/HomeMangas/index.tsx
@@ -1,10 +1,41 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
+import { AllMangas } from "./AllMangas";
+import { useHomeMangas } from "./useHomeMangas";
+import { Header } from "../../Components/Header";
 
 export function MangaHome() {
+
+  
+  const { states } = useHomeMangas()
   return (
-    <Box>
-      <Typography variant="h1">Manga</Typography>
+    <Box sx={{
+      display: "flex",
+      flexDirection: "column",
+      gap: "1.5rem"
+    }}
+    >
+       {/* Starting here Header section */}
+      <Header
+      QueryValue={states.mangaQuery}
+      setSearchBarQueryValue={(e)=> {
+        states.setMangaQuery(e);
+      }}
+      handleSubmit={() => {}}
+      scrollTo="#mangas" 
+      />
+      {/* Finish here Header section */}
+
+      {/* Starting here AllMangas section */}
+      <AllMangas 
+      mangas={states.selectedMangas}
+      paginationData={states.paginationData}
+      onPageChange={(e, page)=> {
+        states.setPageManga(page);
+      }}
+      loading={states.loadingMangas}
+       />
+    {/* Finish here AllMangas section */}
     </Box>
   );
 }
