e2b60d75573a261d220b8b363cc5bf9a12da77b3
Paulo <paulo-augusto12@outlook.com>
2023-05-17 19:18:27 -0300

    refactor: removed unnecessary content from the main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { Box } from "@mui/material";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from "../src/ui/Pages/Home/";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
